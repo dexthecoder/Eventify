4d0c33a73f34c95c18dd0963ec4ac8f4b441d934
dexthecoder <dexthecoder@github.com>
2026-05-20 16:25:11 +0300

    CORS filter

--- src/main/java/com/example/demo/configs/SessionFilter.java
+++ src/main/java/com/example/demo/configs/SessionFilter.java
@@ -29,7 +29,8 @@ public class SessionFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+        response.setHeader("Access-Control-Max-Age", "3600");
 
         // 🔹 CORS preflight isteklerini auth'tan muaf tut
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
